97d996604e6d1d572cdf71c40ff1d8d983a1ee18
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2026-01-12 00:51:17 +0530

    Set up Express server and MongoDB connection; update Vercel configuration

--- api/app.js
+++ api/app.js
@@ -23,6 +23,6 @@ mongoose.connect(process.env.DATABASE_CONNECTION).then(() => {
     console.error("Error connecting to MongoDB", err);
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// });
